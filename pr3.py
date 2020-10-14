900098c676f2db1fd10fe4bc7689a332f99f0fba
JorgeMonzon <jmonzon96@gmail.com>
2020-10-14 10:26:34 +0200

    Ejercicio 5. Este programa ya es capaz de pintar graficos en base a archivos.

--- pr3.py
+++ pr3.py
@@ -8,6 +8,7 @@ Created on Sun Oct 11 21:51:54 2020
 import serial
 import time
 import pandas as pd
+import matplotlib.pyplot as plt
 
 puerto = "COM3" 
 baudio = 115200
@@ -17,6 +18,8 @@ segundos = 5
 esp32 = serial.Serial(puerto, baudio) #Defino el objeto ESP señalando el puerto al que tiene que mirar y la frecuencia de muestreo
 time.sleep(2)                         #Espero 2s para que se acople el puerto serie
 esp32.close()                         #Cierro el puerto para reiniciar el dispositivo
+
+listaTitulos = ["Tiempo", "Aceleración en X", "Aceleración en Y", "Aceleración en Z", "Giroscopo en X", "Giroscopo en Y", "Giroscopo en Z"]
 
 def leeLinea():
     if(esp32.isOpen() == False):
@@ -40,6 +43,19 @@ def calculoDatos(nombreFichero):
     var = df.var()    #Devuelve una lista con la varianza de cada columna
     return med, var
 
+def dibujoGraficaArchivo(nombreFichero):
+    df = pd.read_csv(nombreFichero, ";")
+    ejeX = df.iloc[:,0]     #Valores de la primera columna de pandas
+    (filas, columnas_size) = df.shape
+    for index, columnas in enumerate(df.columns):
+        plt.figure()
+        plt.title(listaTitulos[index])      #El título del grafico
+        ejeY = df.iloc[:,index]
+        #plt.subplot(2, round((columnas_size/2) + 0.5) , index) #Nº de filas, nº de columnas, indice de la figura
+        plt.plot(ejeX, ejeY)
+        plt.show()
+    print("He pintado  las graficas")
+
 escribeFichero("datos.txt", "", "w")    
 escribeFichero("auxiliar.txt", "", "w")
 t_ref = time.time()
@@ -56,6 +72,7 @@ while i < 11:
             linea = leeLinea()
             escribeFichero("datos.txt", linea, "ab")
             escribeFichero("auxiliar.txt", linea, "ab")
+        dibujoGraficaArchivo("datos.txt")
         media5s, varianza5s = calculoDatos("auxiliar.txt")
         #print("Media:", "\n" , media5s)
         #print("Varianza:", "\n" , varianza5s)
